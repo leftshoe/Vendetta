f884d92036ec8d21ba5025457d63f572ec35d129
Aaron <aaron.defazio@gmail.com>
2010-12-02 23:09:00 +1100

    Fixed dragging files into dock icon

--- app/window-manager.js
+++ app/window-manager.js
@@ -7,12 +7,12 @@ app.addEventListener("invoke", function(e) {
 	var file_paths = e.arguments;
 	air.trace("invoke. dir: " + dir + " file_paths " + file_paths);
 
-	// Pass application arguments in
+	// TODO: pass all into one window when tabs are supported
 	if(file_paths && file_paths.length > 0) {
 		for(var i = 0; i < file_paths.length; i++) {
 			var f = dir.resolvePath(file_paths[i]);
 			//TODO: error handling here
-			openWindow(f);
+			openWindow(f.nativePath);
 		}
 	} else {
 		
