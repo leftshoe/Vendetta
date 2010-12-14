95920d601ea503384124fe8529d8d77d4b3a139f
Aaron <aaron.defazio@gmail.com>
2010-12-15 00:13:37 +1100

    Added some padding around to file browser

--- templates/FileBrowser.js
+++ templates/FileBrowser.js
@@ -6,16 +6,25 @@ if (typeof template == 'undefined') { var template = {}; }
 
 template.fileBrowser = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
+  output.append('\t<div class="file-browser-inner">');
+  template.file({file: opt_data.file}, output);
+  output.append('</div>');
+  if (!opt_sb) return output.toString();
+};
+
+
+template.file = function(opt_data, opt_sb) {
+  var output = opt_sb || new soy.StringBuilder();
   output.append('\t');
   if (opt_data.file.isDirectory) {
     output.append('<div class="folder-name ', (opt_data.file.open) ? ' folder-open ' : ' folder-close ', '" data-fullFileName="', soy.$$escapeHtml(opt_data.file.fullFileName), '">', soy.$$escapeHtml(opt_data.file.fileName), '</div>');
     if (opt_data.file.open) {
       output.append('<div class="folder-contents">');
-      var subfileList20 = opt_data.file.files;
-      var subfileListLen20 = subfileList20.length;
-      for (var subfileIndex20 = 0; subfileIndex20 < subfileListLen20; subfileIndex20++) {
-        var subfileData20 = subfileList20[subfileIndex20];
-        template.fileBrowser({file: subfileData20}, output);
+      var subfileList25 = opt_data.file.files;
+      var subfileListLen25 = subfileList25.length;
+      for (var subfileIndex25 = 0; subfileIndex25 < subfileListLen25; subfileIndex25++) {
+        var subfileData25 = subfileList25[subfileIndex25];
+        template.file({file: subfileData25}, output);
       }
       output.append('</div>');
     }
