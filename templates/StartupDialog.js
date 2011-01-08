86b53a0b3ff87606cdcc838408a774882afee4fd
Aaron <aaron.defazio@gmail.com>
2011-01-08 17:37:41 +1100

    Improved usability of startup dialog

--- templates/StartupDialog.js
+++ templates/StartupDialog.js
@@ -6,6 +6,6 @@ if (typeof template == 'undefined') { var template = {}; }
 
 template.startupDialog = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  output.append('\t<div class="startup-info">Press ESC to enter or exit</div><div class="startup-mode-name">Meta-mode</div>');
+  output.append('\t<div class="startup-info">Press ESC to</div><div class="startup-mode-name">enter or exit META-mode</div>');
   if (!opt_sb) return output.toString();
 };
