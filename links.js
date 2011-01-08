c8f56de69ddb0ee62b6e1a7bda4aea3969ffd3e5
Aaron <aaron.defazio@gmail.com>
2011-01-08 23:50:49 +1100

    Initial website commit

---
+++ links.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#logo').click(function() {
+	   window.location = 'index.html';
+    });
+	$('#download').click(function() {
+		window.location = 'https://github.com/downloads/leftshoe/Vendetta/Vendetta-v0-2-1.air';
+	});
+	$('#faq').click(function() {
+		window.location = 'faq.html';
+	});
+	$('#development').click(function() {
+		window.location = 'https://github.com/leftshoe/vendetta';
+	});
+	$('#info').click(function() {
+		window.location = 'info.html';
+	});
+});
